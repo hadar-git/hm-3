ea393feef027bcfa8484388bea8dbb039b1cf267
hadar <hadar01@kazoula.com>
2026-05-27 22:30:58 +0300

    create emp class and add a emp

---
+++ employee.js
@@ -0,0 +1,11 @@
+
+class Emp
+{
+    constructor(name, age, pos) {
+        this.name = name;
+        this.age=age;
+        this.pos=pos;
+    }
+}
+
+module.exports = Emp;
---
+++ index.js
@@ -0,0 +1,7 @@
+
+const Emp = require("./employee");
+
+
+const e1 = new Emp("Moshe", 30, 1);
+const e2 = new Emp("Dana", 25, 2);
+const e3 = new Emp("Yossi", 40, 3);
