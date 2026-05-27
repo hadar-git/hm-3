810b19dcdda3ea1c0f732001066b957cfbadf8a2
hadar <hadar01@kazoula.com>
2026-05-27 23:34:12 +0300

    finished all three employee stages successfully

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-
-const { json } = require("node:stream/consumers");
 const Emp = require("./employee");
 const fs = require('node:fs');
-const { stringify } = require("node:querystring");
 
 const e1 = new Emp("Moshe", 30, 1);
 const e2 = new Emp("Dana", 25, 2);
@@ -10,7 +7,7 @@ const e3 = new Emp("Yossi", 40, 3);
 
 //1 
 try{
- fs.writeFileSync(`emp1.txt`, JSON.stringify(e1, null, 2));
+ fs.writeFileSync(`./employee-data/emp1.txt`, JSON.stringify(e1, null, 2));
     console.log("emp 1 success");
     
 }
@@ -18,4 +15,25 @@ catch (error){
     console.error("emp 1 error", error);
 }
 
+//2
+
+    fs.writeFile(`./employee-data/emp2.txt`, JSON.stringify(e2, null, 2) , (err)=>
+{
+    if(err)
+    { console.log("ERROR", err) ;}
+    else{console.log("great");
+    }
+})
+
+//3
+
+
+ fs.promises.writeFile(`./employee-data/emp3.txt`, JSON.stringify(e3, null, 2))
+.then( ()=> {
+   console.log("third emp is innnnn");
+})
+.catch ( (err)=>   {
+    console.log("nope");
+    
+})
 
